da453dc4c79b16ff66457215c1f441976c9e7a00
dainbrownlow08 <dain.brownlow@outlook.com>
2020-11-18 15:37:25 -0800

    final

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name,grade)
+        if self.roster.key?(grade)
+            self.roster[grade] << name
+        else
+            self.roster[grade] = []
+            self.roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+    def sort
+        self.roster.each{|grade , names| self.roster[grade] = names.sort}
+    end
+
+
+
+
+end
